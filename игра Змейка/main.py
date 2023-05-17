fbdd39c2fef30ccada0b76fc57e4aefe499f8486
PakAlexander <caliostr@gmail.com>
2023-05-18 10:51:43 +1100

    +
    +

---
+++ игра Змейка/main.py
@@ -0,0 +1,14 @@
+import pygame
+
+pygame.init()
+size = (400, 600)
+
+screen =  pygame.display.set_mode(size)
+pygame.display.set_caption("Змейка")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("exit")
+            pygame.quit()
+            exit(0)
