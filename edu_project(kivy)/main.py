5ea2f8817cd6489953a61649a44175c270fe514d
PakAlexander <Caliostr@gmail.com>
2023-05-23 13:56:33 +1100

    in dev

--- edu_project(kivy)/main.py
+++ edu_project(kivy)/main.py
@@ -2,16 +2,44 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.properties import ObjectProperty
+from kivy.core.window import Window
+
+# Window.size = (720, 1280)
+Window.size = (400, 800)
+from kivy.config import Config
+
+Config.set('kivy', 'keyboard_mode', 'systemanddock')
+
+
 class Container(GridLayout):
+    def calculate(self):
+        try:
+            mass = int(self.text_input.text)
+        except:
+            mass = 0
 
-    text_input = ObjectProperty()
-    label_widget = ObjectProperty()
+        inridiets = get_ingr(mass)
 
-    def change_text(self):
-        self.label_widget.text = self.text_input.text.upper()
+        self.salt.text = inridiets.get('salt') + '5'
+        self.solt_nitro.text = inridiets.get('nitro')
+        self.starts.text = inridiets.get('starts')
+        self.sugar.text = inridiets.get("sugar")
+        self.time.text = inridiets.get("time")
+
+def get_ingr(m):
+    nitro = str(10 * m / 1000)
+    salt = str(15 * m / 1000)
+    starts = str(0.5 * m / 1000)
+    sugar= str(5 * m / 1000)
+    salting_time = str(round(m / 500 * 2))
+    return {'nitro': nitro, "salt": salt, "starts": starts, \
+            "sugar": sugar, "time": salting_time}
+
+
 class MyApp(App):
     def build(self):
         return Container()
+
 
 if __name__ == '__main__':
     MyApp().run()
